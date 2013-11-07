e2644bb35df2ca6b78677ff7ce77972acf772bae
Soumya Deb <debloper@gmail.com>
2013-11-08 05:19:15 +0530

    Proof-of-concept d3 hierarchy-tree has been formed - NOT COMMITTING (D3+D3.LAYOUT).JS - Mangling the data to make it consumable by D3 - Processing doesn't return stringified JSON, but parsed Object
    Signed-off-by: Soumya Deb <debloper@gmail.com>

--- script.js
+++ script.js
@@ -26,16 +26,61 @@ vinci.output = function (data) {
     data = JSON.parse(data)
   } catch (e) {
     data = false
-    return data
   }
-  return JSON.stringify(data, null, "\t")
+  return data
 }
 
 // Give output
 document.querySelector("button").addEventListener("click", function () {
   var data = vinci.output(vinci.input())
+
   if (data) {
-    document.querySelector("pre").innerHTML = data
+
+    var payload = []
+
+    for (var i in data) {
+      var tmp = {
+        name: i
+      }
+      delete data[i]
+      payload.push(tmp)
+    }
+
+    data.name = "Viz"
+    data.children = payload
+
+    document.querySelector("pre").innerHTML = JSON.stringify(data, null, "\t")
+
+    var tree = d3.layout.tree().size([300,150])
+      , svg = d3.select("#viz").append("svg")
+                .attr("width", 640)
+                .attr("height", 640)
+                .append("g")
+                .attr("transform", "translate(40, 0)")
+
+      , diagonal = d3.svg.diagonal()
+                    .projection(function(d) { return [d.y, d.x] })
+
+      , nodes = tree.nodes(data)
+
+      , links = tree.links(nodes)
+
+      , link = svg.selectAll("pathlink")
+                    .data(links).enter().append("path")
+                    .attr("class", "link").attr("d", diagonal)
+
+      , node = svg.selectAll("g.node")
+                    .data(nodes).enter().append("g")
+                    .attr("transform", function(d) {
+                      return "translate(" + d.y + "," + d.x + ")"
+                    })
+
+      node.append("circle").attr("r", 3.5)
+      node.append("text")
+          .attr("dx", function(d) { return d.children ? -8 : 8 })
+          .attr("dy", 3)
+          .attr("text-anchor", function(d) { return d.children ? "end" : "start" })
+          .text(function(d) { return d.name })
   } else {
     document.querySelector("pre").innerHTML = "Input could not be parsed as JSON"
   }
