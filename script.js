beef19542c5e5fa06c248fc3ffe2bfcf2b25bca5
Soumya Deb <debloper@gmail.com>
2013-11-07 17:38:59 +0530

    Drag & Drop implemented - Allows dropping text files in the TESTAREA - FileReader API to read & extract file content
    Signed-off-by: Soumya Deb <debloper@gmail.com>

--- script.js
+++ script.js
@@ -6,6 +6,20 @@ vinci.input = function () {
   return document.querySelector("textarea").value
 }
 
+document.querySelector("textarea").addEventListener("drop", function (event) {
+  event.preventDefault()
+
+  var file = event.dataTransfer.files[0]
+    , reader = new FileReader()
+
+  reader.addEventListener("load", function (evt) {
+    document.querySelector("textarea").value = evt.target.result
+  })
+
+  reader.readAsText(file)
+}, false)
+
+
 // Process data
 vinci.output = function (data) {
   try {
